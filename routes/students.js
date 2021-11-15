89a7680c6b0b06ad94a53c95294e7bb4f03403fe
tmoussa01 <tmoussa-01@hotmail.com>
2021-11-15 14:46:39 +0000

    add db_connect.js + delete student from id

--- routes/students.js
+++ routes/students.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const debug = require('debug')('monprojetdemo:api:student');
-const connection = require('../db_connect');
+const connection = require('../db_connect').connection;
 const studentList = [];
 
 /* GET users listing. */
@@ -50,6 +50,22 @@ router.get('/:id', (req, res) => {
         } else {
           res.status(404).send("Student not found");
         }
+      }
+    }
+  );
+});
+
+router.delete('/:id', (req, res) => {
+  debug("Delete student");
+  const studentId = req.params.id;
+  connection.query(
+    'DELETE FROM `students` WHERE id = ?',
+    [ studentId ],
+    (err, results, fields) => {
+      if (err) {
+        debug(err);
+      } else {
+        res.status(204).send();
       }
     }
   );
