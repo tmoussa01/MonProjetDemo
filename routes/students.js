1a30386b5e960f9e1ab421b2f94273a02bf9b85b
tmoussa01 <tmoussa-01@hotmail.com>
2021-11-15 15:41:18 +0000

    student.ejs

--- routes/students.js
+++ routes/students.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
         debug(err);
       } else {
         res.json(results);
+        res.render('student', { results: results});
       }
     }
   );
